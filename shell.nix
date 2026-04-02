df4c7681c374356d0012fdb7a05bda9a3cd55441
Gustavo Cruz <cru22779@uvg.edu.gt>
2026-04-01 22:58:43 -0600

    [CHORE] added backend tools

--- shell.nix
+++ shell.nix
@@ -2,7 +2,20 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    # Frontend
     nodejs
     nodePackages.npm
+
+    # Backend
+    python3
+    python3Packages.fastapi
+    python3Packages.uvicorn
+    python3Packages.sqlalchemy
+    python3Packages.psycopg2
+    python3Packages.pydantic
+    python3Packages.passlib
+    python3Packages.bcrypt
+    python3Packages.python-jose
+    python3Packages.multipart
   ];
 }
